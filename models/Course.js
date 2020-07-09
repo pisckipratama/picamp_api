3e1a5f66c7bff066b3c33b7c0817fec574f54152
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-09 15:28:35 +0700

    add aggregate for averageCost

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const colors = require('colors');
 
 const CourseSchema = new mongoose.Schema({
   title: {
@@ -36,6 +37,40 @@ const CourseSchema = new mongoose.Schema({
     ref: 'Bootcamp',
     required: true
   }
+});
+
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log('calculation avg const...'.cyan);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    })
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// call getAverageCost after save
+CourseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+CourseSchema.pre('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
