f9e94c96a5d905ce10e839a4a1cdf596f1d2f290
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-12 18:44:23 +0700

    add localhost mongo uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/devcamper';
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI, {
+  const conn = await mongoose.connect(uri, {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
