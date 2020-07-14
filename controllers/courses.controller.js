befcf648d2b639ea0cd827df97459534d161e738
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-14 09:45:55 +0700

    add course ownership

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -39,9 +39,16 @@ const getSingleCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 const createCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) return next(new ErrorHandler(`No resource with the id of '${req.params.bootcampId}'`, 404));
+
+  // make sure is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorHandler(`User ${req.user.id} is not authorized to add a course to ${bootcamp._id}`, 401));
+  }
+
   const course = await Course.create(req.body);
   res.status(201).json({ success: true, data: course });
 });
@@ -53,6 +60,12 @@ const updateCourse = asyncHandler(async (req, res, next) => {
   let course = await Course.findById(req.params.id);
 
   if (!course) return next(new ErrorHandler(`No resource with the id of '${req.params.id}'`, 404));
+
+  // make sure is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorHandler(`User ${req.user.id} is not authorized to update a course`, 401));
+  }
+
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
@@ -67,6 +80,11 @@ const deleteCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id);
 
   if (!course) return next(new ErrorHandler(`No resource with the id of '${req.params.id}'`, 404));
+
+  // make sure is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorHandler(`User ${req.user.id} is not authorized to delete a course`, 401));
+  }
 
   await course.remove();
   res.status(200).json({ success: true, data: {} });
