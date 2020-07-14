22ba16dbc9a93e7cf7622303e01b04a3c575416d
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-14 17:05:32 +0700

    add nodemailer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const ErrorHandler = require('../helpers/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../helpers/sendEmail');
 const User = require('../models/User');
 const ErrorResponse = require('../helpers/errorResponse');
 
@@ -95,10 +96,34 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
   
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user
-  });
+  // create reset Url
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving email beacause you (or someone else) has requested the reset of a password.
+  Please make a PUT request to: \n\n ${resetURL}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password Reset Token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email Sent!' });
+  } catch (error) {
+    console.error(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent!', 500));
+  };
+
+  // res.status(200).json({
+  //   success: true,
+  //   data: user
+  // });
 });
 
 module.exports = {
---
+++ helpers/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL, // generated ethereal user
+      pass: process.env.SMTP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  const message = await transporter.sendMail({
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  });
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+}
+
+module.exports = sendEmail;
