447333fd2597dd95e584dbe0ca2450673a584004
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-19 17:33:18 +0700

    add update user details and password function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -59,7 +59,44 @@ const getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: user
-  })
+  });
+});
+
+// @desc    Update user details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  Private
+const updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// @desc    Update password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  Private
+const updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Forgot Password
@@ -164,5 +201,7 @@ module.exports = {
   loginUser,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
+  updatePassword
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { registerUser, loginUser, getMe, forgotPassword, resetPassword } = require('../controllers/auth.controller');
+const { registerUser, loginUser, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth.controller');
 const router = express.Router();
 const { protects } = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protects, getMe);
+router.put('/updatedetails', protects, updateDetails);
+router.put('/updatepassword', protects, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
